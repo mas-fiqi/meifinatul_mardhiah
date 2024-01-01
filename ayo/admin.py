4904dfeec24292b65b6224fe4293e709abcc6716
mas-fiqi <moh.rafiqi.wahyudi@mail.com>
2024-01-01 12:17:45 +0700

    tambah admin

--- ayo/admin.py
+++ ayo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Data
 # Register your models here.
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ("Nama_Lengkap", "Panggilan")
+
+admin.site.register(Data, DataAdmin)
--- ayo/models.py
+++ ayo/models.py
@@ -1,3 +1,9 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Data(models.Model):
+    Nama_Lengkap = models.CharField(max_length=100)
+    Panggilan = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.Panggilan}, {self.Nama_Lengkap}"
